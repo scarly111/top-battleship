ec36d3ef423370ef2a5df8b9ee3cadba3ca02b67
ScarLi <yigitcagri48@gmail.com>
2025-08-27 13:41:20 +0300

    Ship class created with the desired functions

---
+++ Ship.js
@@ -0,0 +1,18 @@
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+  }
+
+  hit() {
+    if (this.hits < this.length) {
+      this.hits += 1;
+    }
+  }
+
+  isSunk() {
+    return this.hits >= this.length;
+  }
+}
+
+module.exports = Ship;
