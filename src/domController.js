7e09cd6e28aa3ed0e3eb3064aae61d05ceda0f83
ScarLi <yigitcagri48@gmail.com>
2025-08-28 11:31:17 +0300

    render board

--- src/domController.js
+++ src/domController.js
@@ -7,4 +7,37 @@ const domController = (() => {
         renderBoard(playerBoardEl, human.gameboard, true);
         renderBoard(enemyBoardEl, computer.gameboard, false);
     }
+
+    function renderBoard(container, gameboard, isPlayer) {
+        container.innerHTML = '';
+        for (let y = 0; y < gameboard.size; y++) {
+            for (let x = 0; x < gameboard.size; x++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+
+                const item = gameboard.board[y][x];
+
+                if (isPlayer) {
+                    if (item) cell.classList.add('ship');
+                } else {
+                    if (item && item.hits > 0) {
+                        cell.classList.add('hit');
+                    }
+                }
+
+                // Missing shots
+                if (gameboard.missedAttacks.some(pos => pos[0] === x && pos[1] === y)) {
+                    cell.classList.add('miss');
+                }
+
+                // Dataset for enemy
+                if (!isPlayer) {
+                    cell.dataset.x = x;
+                    cell.dataset.y = y;
+                }
+
+                container.appendChild(cell);
+            }
+        }
+    }
 })
